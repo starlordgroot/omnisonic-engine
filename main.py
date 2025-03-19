436e04617b6a01df286d51645d02e384b3e39120
David Wise <dwise5@students.towson.edu>
2025-03-19 00:38:53 -0400

    adding audio processing logic

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from fastapi import FastAPI
 from src.api.routes import router as api_router
 from src.api.websocket import router as ws_router
 import numpy as np
-import sounddevice as sd
 from src.utils.frequency_helper import generate_harmonic_frequencies
-from src.services.audio_processing import blend_frequencies, play_tone
+import src.services.audio_processing as processor
 
 # Create the FastAPI application instance
 app = FastAPI(title="OmniSonic Core")
@@ -21,14 +20,28 @@ async def root():
     return {"message": "OmniSonic Backend Running"}
 
 #Plays a frequency
+@app.post("/harmonize")
+async def play_harmony_endpoint(frequency: float, duration: float = 1.0):
+    """Endpoint to play a harmony."""
+    sample_rate = 44100
+
+    base_wave = processor.generate_frequency(frequency)
+
+    combined_waves = generate_harmonic_frequencies(frequency)
+
+    harmony_waves = processor.blend_frequencies(combined_waves)
+
+    processor.play_tone(harmony_waves, duration)
+    return {"message": f"Playing tone at {frequency} Hz for {duration} seconds"}
+
+#Plays a frequency
 @app.post("/play-tone")
 async def play_tone_endpoint(frequency: float, duration: float = 1.0):
     """Endpoint to play a tone."""
     sample_rate = 44100
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
     wave = 0.5 * np.sin(2 * np.pi * frequency * t)
-    sd.play(wave, samplerate=sample_rate)
-    sd.wait()
+    processor.play_tone(wave, duration)
     return {"message": f"Playing tone at {frequency} Hz for {duration} seconds"}
 
 # Run the application if executed directly
